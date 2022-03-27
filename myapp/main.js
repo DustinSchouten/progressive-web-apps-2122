5e622bfb025b20547fdfe3dbd0a3e6cb15c52ec8
DustinSchouten <dustinschouten2@gmail.com>
2022-03-27 11:27:44 +0200

    first_app_version_finished_and_installation_documented_in_readme.md

---
+++ myapp/main.js
@@ -0,0 +1,64 @@
+require('dotenv').config()
+const express = require('express')
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+    res.render('index', {
+        pageTitle: 'homepage',
+        quote: {text: 'The app for the best and most inspirational quotes!'},
+        quote_text_fontsize: '1.75em',
+    })
+})
+
+
+app.get('/quotes', function (req, res) {
+    fetchJson('https://quote.api.fdnd.nl/v1/quote').then(function (jsonData) {
+        jsonData = jsonData.filter((item) => {
+            return item.text.length > 10
+        })
+        res.render('quotes', {
+            pageTitle: 'overviewpage',
+            data: jsonData,
+        })
+    })
+})
+
+
+app.get('/quote/:id', function (req, res) {
+    fetchJson(`https://quote.api.fdnd.nl/v1/quote`).then(function (jsonData) {
+        let id = req.params.id.split(':')[1];
+        let quote_text_length = jsonData[id].text.length;
+        let quote_text_fontsize = '1.75em';
+        if (quote_text_length > 100) {
+            quote_text_fontsize = '1.4em';
+        }
+        let image_avatar = (jsonData[id].avatar || '/images/image_skeleton.png');
+        res.render('quote', {
+            pageTitle: 'quotepage',
+            quote: jsonData[id],
+            quote_text_fontsize: quote_text_fontsize,
+            image_avatar: image_avatar,
+        })
+    })
+})
+
+
+app.set('port', process.env.PORT || 8000)
+
+const server = app.listen(app.get('port'), function () {
+  console.log(`Application started on port: ${app.get('port')}`)
+})
+
+async function fetchJson(url) {
+    return await fetch(url)
+        .then((response) => response.json())
+        .then((body) => body.data)
+        .catch((error) => error)
+}
